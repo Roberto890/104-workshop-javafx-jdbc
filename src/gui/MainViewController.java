e19ace8b8bd7c3c16be2a11c94d208a499993fb9
Roberto Jesus <roberto.jesus.amaral@gmail.com>
2020-08-14 19:42:32 -0300

    About view - correct

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -4,14 +4,17 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.Scene;
-
-import gui.util.Alerts;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -47,17 +50,17 @@ public class MainViewController implements Initializable{
 	public void initialize(URL url, ResourceBundle rb) {
     }
     
-    //essa aplicação grafica é multitread para nao interromper para nao dar nada de estranho usa synchronized
+    //essa aplicacao grafica te, multitread para nao interromper para nao dar nada de estranho usa synchronized
     private synchronized void loadView(String absoluteName){
         try{
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             VBox newVBox = loader.load();
 
-            Scene mainScene = Main.getMainScene();
+            Scene mainScene = Main.getMainScene();	
             //pegando referencia para o vbox
             //mainScene getRoot - pega o primeiro elemento da minha View no caso dessa é o ScrollPane
             //o get content é o que esta dentro do scrollpane no caso o VBox
-            VBox mainVBox = (Vbox) ((ScrollPane)mainScene.getRoot()).getContent();
+            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
 
             //quardando referencia do menu - pega o primeiro filho do vbox da janela principal no caso o mainMenu
             Node mainMenu = mainVBox.getChildren().get(0);
@@ -67,7 +70,7 @@ public class MainViewController implements Initializable{
             //adiciona o mainMenu
             mainVBox.getChildren().add(mainMenu);
             //adiciona os filhos do newVBOX
-            mainVBox.getChildren().addAll(newVBox.getChildren);
+            mainVBox.getChildren().addAll(newVBox.getChildren());
 
         } catch (IOException e){
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,10 +1,10 @@
 package gui.util;
 
-import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Alert;
 
 public class Alerts {
-
+	
 	public static void showAlert(String title, String header, String content, AlertType type) {
 		Alert alert = new Alert(type);
 		alert.setTitle(title);
