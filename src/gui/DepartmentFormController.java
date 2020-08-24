ba2d76713a16570ccbe2251a7a9f571a8f4984a4
Roberto Jesus <roberto.jesus.amaral@gmail.com>
2020-08-24 20:23:01 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,18 +3,27 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 	
 	//faz uma dependencia do department para fazer o inserts e updates
 	private Department entity;
+	
+	//dependencia com o department service
+	private DepartmentService service; 
 	
 	@FXML
 	private TextField txtId;
@@ -31,20 +40,51 @@ public class DepartmentFormController implements Initializable {
 	@FXML 
 	private Button btCancel;
 	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
-	}
-	
-	@FXML
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
-	}
-	
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
 	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		try {
+			//pega os dados que estao nos campos e coloca no entity
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			//fecha a janela depois do update
+			Utils.currentStage(event).close();
+			
+		}catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj; 
+		
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		//fecha a janela depois do update
+		Utils.currentStage(event).close();
+	}
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rs) {
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -14,4 +14,12 @@ public class DepartmentService {
 		//Vai no banco e pega os departamentos
 		return dao.findAll();
 	}
+	
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}else {
+			dao.update(obj);
+		}
+	}
 }
