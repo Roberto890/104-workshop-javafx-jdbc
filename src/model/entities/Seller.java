3a259e819f29a0109912c87b2dd7cd022abe5c7e
Roberto Jesus <roberto.jesus.amaral@gmail.com>
2020-08-29 10:42:16 -0300

    Seller TableView

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -10,7 +10,7 @@ public class Seller implements Serializable{
 	private Integer id;
 	private String name;
 	private String email;
-	private Date bitrhDate;
+	private Date birthDate;
 	private Double baseSalary;
 	
 	private Department department;
@@ -19,11 +19,11 @@ public class Seller implements Serializable{
 		
 	}
 
-	public Seller(Integer id, String name, String email, Date bitrhDate, Double baseSalary, Department department) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
-		this.bitrhDate = bitrhDate;
+		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
 		this.department = department;
 	}
@@ -52,12 +52,12 @@ public class Seller implements Serializable{
 		this.email = email;
 	}
 
-	public Date getBitrhDate() {
-		return bitrhDate;
+	public Date getBirthDate() {
+		return birthDate;
 	}
 
-	public void setBitrhDate(Date bitrhDate) {
-		this.bitrhDate = bitrhDate;
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
 	}
 
 	public Double getBaseSalary() {
@@ -103,7 +103,7 @@ public class Seller implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", bitrhDate=" + bitrhDate + ", baseSalary="
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
 	
