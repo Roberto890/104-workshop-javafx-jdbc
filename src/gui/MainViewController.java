a7000b2ece32b34506cb419ec8165b18ff7083ac
Roberto Jesus <roberto.jesus.amaral@gmail.com>
2020-08-29 10:24:38 -0300

    Seller List

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewController implements Initializable{
 
@@ -34,7 +35,10 @@ public class MainViewController implements Initializable{
 	//teste para clique nas opcoes do menu
 	@FXML
 	public void onMenuItemSellerAction() {
-		System.out.println("onMenuItemSellerAction clicado");
+		loadView("/gui/SellerList.fxml",(SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
